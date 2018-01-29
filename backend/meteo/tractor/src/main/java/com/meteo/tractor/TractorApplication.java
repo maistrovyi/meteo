717345c7fdb0dc570b30fdaedf483f70bb336d9c
Volodymyr Maystrovyy <maystrovyy@gmail.com>
2018-01-30 01:30:00 +0200

    Add Tractor module with dark-sky dependency.

---
+++ backend/meteo/tractor/src/main/java/com/meteo/tractor/TractorApplication.java
@@ -0,0 +1,13 @@
+package com.meteo.tractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TractorApplication.class, args);
+	}
+
+}
